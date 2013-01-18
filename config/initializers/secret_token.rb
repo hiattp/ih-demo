ed346ccccb2dccd924b0cb2b17c20257f9947f31
Paul Hiatt <hiattp@gmail.com>
2013-01-17 21:03:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfohoundsDemo::Application.config.secret_token = 'e32f3951eee973579400f903c1817060c33748ab8c502427f5418ad55f84744bc786a217317245951ee2c8c0cf2220d9e7f872661e16ba627f48b716185adbe9'
