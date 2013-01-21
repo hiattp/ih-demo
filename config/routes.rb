55a436db2f83b06224e54c1123f8adfb2d377cb7
Paul Hiatt <hiattp@gmail.com>
2013-01-20 22:33:57 -0500

    added welcome#index as homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ InfohoundsDemo::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  match 'home' => 'welcome#index'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
@@ -48,7 +48,7 @@ InfohoundsDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
