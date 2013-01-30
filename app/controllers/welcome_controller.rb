505b01b8d5c69e8d0bff8dc57821faf5af3733eb
Paul Hiatt <hiattp@gmail.com>
2013-01-30 02:17:09 -0500

    added check for not null in index page user list

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     User.create(:first_name => params["first_name"], :last_name => params["last_name"])
-    @users = User.all
+    # @users = User.all
+    @users = User.where("first_name IS NOT NULL AND last_name IS NOT NULL")
   end
 end
