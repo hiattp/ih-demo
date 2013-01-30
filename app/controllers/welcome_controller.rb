1f856db4f9f159bd1529c6b92984803e9ca380df
Paul Hiatt <hiattp@gmail.com>
2013-01-30 01:55:02 -0500

    added part 3

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    User.create(:first_name => params["first_name"], :last_name => params["last_name"])
+    @users = User.all
   end
 end
