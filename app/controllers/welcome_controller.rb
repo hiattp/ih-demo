d785c473ac63da9db0f0829d01cced74cf81123c
dmipham <diemmipham@gmail.com>
2013-02-07 09:05:49 -0500

    final code for part 3 that works

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    User.create(:first_name => "Diemmi", :last_name => "Pham")
-    @users = User.all
+    User.create(:first_name => params["first_name"], :last_name => params["last_name"])
+    @users = User.where("first_name IS NOT NULL AND last_name is NOT NULL")
   end
 end
